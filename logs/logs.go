7eaebff924877127af23cd724f6f4bec40291adf
Ivan Zakutniy <ivan.zakutny@flant.com>
2023-05-09 16:03:56 +0400

    config print update

--- logs/logs.go
+++ logs/logs.go
@@ -25,18 +25,29 @@ func StartUp(appConfig utils.Config, logger hclog.Logger) {
 
 func printConfig(s interface{}) string {
 	hiddenFields := []string{"DeletionRegexp"}
-	structType := reflect.TypeOf(s)
 	structValue := reflect.ValueOf(s)
 
 	config := fmt.Sprintf("\n\n%v:\n", "Loaded config")
+	printFields(structValue, hiddenFields, "\t", &config)
+
+	config += "\n The config options is described in the repository's README.md"
+	return config
+}
+
+func printFields(value reflect.Value, hiddenFields []string, indent string, config *string) {
+	structType := value.Type()
 	for i := 0; i < structType.NumField(); i++ {
 		field := structType.Field(i)
-		value := structValue.Field(i)
 		if utils.IsContains(hiddenFields, field.Name) {
 			continue
 		}
-		config += fmt.Sprintf("\t%s: %v\n", field.Name, value.Interface())
+		fieldValue := value.Field(i)
+		switch fieldValue.Kind() {
+		case reflect.Struct:
+			*config += fmt.Sprintf("%s%s:\n", indent, field.Name)
+			printFields(fieldValue, hiddenFields, indent+"\t", config)
+		default:
+			*config += fmt.Sprintf("%s%s: %v\n", indent, field.Name, fieldValue.Interface())
+		}
 	}
-
-	return config
 }
